f54ae9874f09f6ee5ff9fd3bf59d61ec6c255ce4
luckyevil300-web <lucky.evil300@gmail.com>
2026-06-09 11:50:00 -0400

    Update nativemusicplayer.kt

--- android-plugin/src/main/kotlin/com/foxblack/nativemusicplayer/nativemusicplayer.kt
+++ android-plugin/src/main/kotlin/com/foxblack/nativemusicplayer/nativemusicplayer.kt
@@ -1,3 +1,9 @@
 package  com.foxblack.nativemusicplayer
 class nativemusicplayer  {
+    fun ping(): String {
+        return "pong"
+    }
+    fun version(): String {
+        return "0.0.1"
+    }
 }
