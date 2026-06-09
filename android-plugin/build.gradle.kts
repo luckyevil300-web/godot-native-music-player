6ec2ac304b156f4877fe3922999f86000fe222b6
luckyevil300-web <lucky.evil300@gmail.com>
2026-06-09 10:21:25 -0400

    Update build.gradle.kts

--- android-plugin/build.gradle.kts
+++ android-plugin/build.gradle.kts
@@ -1,1 +1,20 @@
+plugins {
+    id("com.android.library") version "8.5.2"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
+}
 
+android {
+    namespace = "com.foxblack.nativemusicplayer"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+}
