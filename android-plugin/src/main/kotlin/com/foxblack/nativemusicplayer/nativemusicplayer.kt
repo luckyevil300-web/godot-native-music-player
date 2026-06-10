263748521a4457ba628a77c35e3d98ff3a229737
luckyevil300-web <lucky.evil300@gmail.com>
2026-06-10 10:47:11 -0400

    Add ping and version methods to NativeMusicPlayer

--- android-plugin/src/main/kotlin/com/foxblack/nativemusicplayer/nativemusicplayer.kt
+++ android-plugin/src/main/kotlin/com/foxblack/nativemusicplayer/nativemusicplayer.kt
@@ -2,6 +2,7 @@ package  com.foxblack.nativemusicplayer
 
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
+import org.godotengine.godot.plugin.UsedByGodot
 
 class NativeMusicPlayer(
     godot: Godot
@@ -10,4 +11,14 @@ class NativeMusicPlayer(
     override fun getPluginName(): String {
         return "NativeMusicPlayer"
     }
+    
+    @UsedByGodot
+    fun ping(): String {
+        return "pong"
+    }
+
+    @UsedByGodot
+    fun version(): String {
+        return "0.0.1"
+    }    
 }    
